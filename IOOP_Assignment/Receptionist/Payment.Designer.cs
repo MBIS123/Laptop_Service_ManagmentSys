4a86c76466d488cb945c33c560d3a0faa2dcd4d2
CHEW YONG EN <TP064861@mail.apu.edu.my>
2022-01-28 10:51:45 +0800

    no changes

--- IOOP_Assignment/Receptionist/Payment.Designer.cs
+++ IOOP_Assignment/Receptionist/Payment.Designer.cs
@@ -222,7 +222,7 @@
             // 
             this.dataGridView1.AllowUserToDeleteRows = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(240, 98);
+            this.dataGridView1.Location = new System.Drawing.Point(238, 51);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
             this.dataGridView1.RowHeadersWidth = 62;
