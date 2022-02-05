3784ed30d24876b51d68754bc44b134ceee5d857
jayren17 <TP064976@mail.apu.edu.my>
2022-02-05 15:13:53 +0800

    changed namespaces + created users class

---
+++ IOOP_Assignment/Users.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IOOP_Assignment
+{
+    internal class Users
+    {
+        private string username;
+        private string password;
+
+        public User(string u, string p) //u = admin, p = admin
+        {
+            username = u;
+            password = p;
+        } 
+
+        public string login(string un) //login method, un = admin
+        {
+            string status = null;
+
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["myCS"]);
+        }
+
+    }
+}
