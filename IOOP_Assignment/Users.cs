5a14384e2a26daa836e26a449670a57624f1de5f
jayren17 <TP064976@mail.apu.edu.my>
2022-02-05 20:25:16 +0800

    almost completed user class + changed namespace for LogIn form

--- IOOP_Assignment/Users.cs
+++ IOOP_Assignment/Users.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using IOOPAssignment;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -12,7 +14,7 @@ namespace IOOP_Assignment
         private string username;
         private string password;
 
-        public User(string u, string p) //u = admin, p = admin
+        public Users(string u, string p) //u = admin, p = admin
         {
             username = u;
             password = p;
@@ -22,7 +24,44 @@ namespace IOOP_Assignment
         {
             string status = null;
 
-            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["myCS"]);
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["myCS"].ToString());
+            con.Open();
+
+            //SqlCommand objectName = new Constructor(sqlQuery, connectionString)
+            SqlCommand cmd = new SqlCommand("select count(*) from users where username = '" + username + "' and password = '" + password + "'", con);
+
+            int count = Convert.ToInt32(cmd.ExecuteScalar().ToString());
+
+            if (count > 0) //redirection to admin or student
+            {
+                SqlCommand cmd2 = new SqlCommand("select role from users where username = '" + username + "' and password = '" + password + "'", con);
+                string user_role = cmd2.ExecuteScalar().ToString();
+
+                if (user_role = "admin")
+                {
+                    AdminPage a = new AdminPage();
+                    a.ShowDialog();
+                }
+                /*else if (user_role = "customer")
+                {
+                    wenhui, you need to change your namespace to IOOP_Assignment first. 
+                }*/
+                else if (user_role = "receptionist")
+                {
+                    frmLogin f = new frmLogin();
+                    f.ShowDialog();
+                }
+                else if (user_role = "technician")
+                {
+                    TechnicianDashboard t = new TechnicianDashboard();
+                    t.ShowDialog();
+                }
+                else
+                    status = "Incorrect username or password entered!";
+                con.Close();
+
+                return status;
+            }    
         }
 
     }
