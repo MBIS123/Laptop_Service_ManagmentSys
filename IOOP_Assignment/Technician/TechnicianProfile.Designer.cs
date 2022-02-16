ac768bc1e862d5e7ca68f24671435a0bacb7daf6
jayren17 <TP064976@mail.apu.edu.my>
2022-02-16 14:55:45 +0800

    finalizing update technician profile and technician profile sizes

--- IOOP_Assignment/Technician/TechnicianProfile.Designer.cs
+++ IOOP_Assignment/Technician/TechnicianProfile.Designer.cs
@@ -181,7 +181,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft JhengHei UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label2.Location = new System.Drawing.Point(251, 22);
+            this.label2.Location = new System.Drawing.Point(273, 60);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(104, 36);
             this.label2.TabIndex = 10;
